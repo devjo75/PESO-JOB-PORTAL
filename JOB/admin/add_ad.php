bf888dc6f22e5b9dc8e8358800a847257531cd52
bryllemitra <brylleina67@gmail.com>
2025-02-27 16:20:43 +0800

    delete space

--- JOB/admin/add_ad.php
+++ JOB/admin/add_ad.php
@@ -3,7 +3,6 @@ include '../includes/config.php';
 include '../includes/header.php';
 include '../includes/restrictions.php';
 
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $title = $_POST['title'];
     $description = $_POST['description'];
